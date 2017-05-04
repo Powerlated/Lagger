bfeee1115865e249841c4bea98bca3487bbf5069
Powerlated <zjia08@gmail.com>
2017-05-03 21:38:09 -0400

    first

---
+++ src/Lagger.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Brian on 5/3/2017.
+ */
+public class Lagger {
+    static int threads = Runtime.getRuntime().availableProcessors();
+    public static void main(String[] args) {
+        System.out.println(threads + " threads detected");
+        System.out.println("Starting lagger in 10 seconds...");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int counter = 0;
+        while (counter != threads) {
+            new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    while (true) {
+                        System.out.println("YOLO FROM " + Thread.currentThread().getName().toUpperCase());
+                    }
+                }
+            });
+        }
+    }
+}
